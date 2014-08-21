4479f982d976d4dbff22b8ac4aa387a86b060d00
Geoff MacDonald <gmacdonald@pivotallabs.com>
2014-08-21 16:18:28 -0400

    initial commit

---
+++ GMMSplashImageUnveilingTransition/GMMSplashImageUnveilingTransition.h
@@ -0,0 +1,19 @@
+//
+//  GMMSplashImageUnveilingTransition.h
+//  GMMSplashImageUnveilingTransition
+//
+//  Created by Xtreme Dev on 2014-08-21.
+//  Copyright (c) 2014 GeoffMacDonald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMMSplashImageUnveilingTransition.
+FOUNDATION_EXPORT double GMMSplashImageUnveilingTransitionVersionNumber;
+
+//! Project version string for GMMSplashImageUnveilingTransition.
+FOUNDATION_EXPORT const unsigned char GMMSplashImageUnveilingTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMMSplashImageUnveilingTransition/PublicHeader.h>
+
+
